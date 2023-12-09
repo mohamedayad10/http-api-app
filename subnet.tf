c2f566618543cdf29168fce29948380ddf99ffe1
mohamedmahmoud <70040995+mohamedayad10@users.noreply.github.com>
2023-12-09 14:32:28 +0200

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -4,6 +4,12 @@ resource "aws_subnet" "public-subnet" {
   availability_zone       = "${var.region}a"
   map_public_ip_on_launch = true
 }
+resource "aws_subnet" "public-subnet-2" {
+  vpc_id                  = aws_vpc.dev-vpc.id
+  cidr_block              = var.public-subnet-2
+  availability_zone       = "${var.region}c"
+  map_public_ip_on_launch = true
+}
 resource "aws_subnet" "private-subnet" {
   vpc_id            = aws_vpc.dev-vpc.id
   cidr_block        = var.private-subnet
